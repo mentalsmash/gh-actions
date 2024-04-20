25bde05b204fa65ca966eb8d992fc29bd7ed8d2a
Andrea Sorbini <asorbini@rti.com>
2024-04-20 00:44:01 -0700

    Adjust context access

--- .github/workflows_pyconfig/settings.py
+++ .github/workflows_pyconfig/settings.py
@@ -138,7 +138,7 @@ def settings(cfg: NamedTuple, github: NamedTuple) -> dict:
 
   badge_cfg = getattr(cfg.release.badges, build_label)
   badge_filename = github.repository.replace("/", "-") + "-badge-" + build_label
-  badge_base_image = badge_cfg.base_img._as_dict()
+  badge_base_image = badge_cfg.base_image._as_dict()
   badge_base_image["message"] = cfg.release.base_image
   badge_base_image["filename"] = f"{badge_filename}-base-image.json"
   badge_version = badge_cfg.version._as_dict()
