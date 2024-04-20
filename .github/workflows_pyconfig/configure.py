1e5d8ee7ae97b4aa9e8a478bb69231a9462d9489
Andrea Sorbini <asorbini@rti.com>
2024-04-20 01:14:59 -0700

    debugging

--- .github/workflows_pyconfig/configure.py
+++ .github/workflows_pyconfig/configure.py
@@ -137,7 +137,7 @@ def configure(
       action_outputs[var] = _select_attribute(ctx, ctx_select)
 
   if workflow:
-    workflow_mod = importlib.import_module(f"workflows.{workflow}")
+    workflow_mod = importlib.import_module(f"workflows_pyconfig.workflows.{workflow}")
     dyn_outputs = workflow_mod.configure(cfg=cfg, github=github, inputs=inputs)
     if dyn_outputs:
       action_outputs.update(dyn_outputs)
