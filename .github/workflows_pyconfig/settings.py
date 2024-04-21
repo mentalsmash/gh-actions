6c021cb125fbc0e8aa67106a803b6d684b7a66d0
Andrea Sorbini <asorbini@rti.com>
2024-04-20 18:15:42 -0700

    Rebuild images when settings change

--- .github/workflows_pyconfig/settings.py
+++ .github/workflows_pyconfig/settings.py
@@ -119,7 +119,7 @@ def settings(clone_dir: Path, cfg: NamedTuple, github: NamedTuple) -> dict:
   )
   admin_build_platforms_config = ",".join(cfg.ci.images.admin.build_platforms)
 
-  release_base_images = [release_cfg.base_image for release_cfg in cfg.release.profiles]
+  release_base_images = list({release_cfg.base_image for release_cfg in cfg.release.profiles})
 
   tester_base_images_matrix = json.dumps(release_base_images)
   tester_registries = extract_registries(
