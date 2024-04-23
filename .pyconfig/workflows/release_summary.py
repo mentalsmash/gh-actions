e264e7f8d97f29dc9b6be05f9685ebfc68d42fd9
Andrea Sorbini <asorbini@rti.com>
2024-04-23 13:57:09 -0700

    Release summary

--- .pyconfig/workflows/release_summary.py
+++ .pyconfig/workflows/release_summary.py
@@ -15,88 +15,181 @@
 ###############################################################################
 from pathlib import Path
 from typing import NamedTuple
+import json
+from fnmatch import fnmatch
+from functools import chain
+
+
+def _image_link(github: NamedTuple, cfg: NamedTuple, image: str) -> str:
+  image_repo = image.split(":")[0]
+  repo_name = image_repo.split("/")[-1]
+  if image.startswith("ghcr.io/"):
+    url = f"{cfg.build.repository.url}/pkgs/container/{repo_name}"
+  elif image.startswith(github.repository):
+    url = f"https://hub.docker.com/r/{image_repo}"
+  else:
+    # unknown registry
+    url = f"https://{image_repo}"
+  return f"[`{image}`]({url})"
+
+
+def _deb_pkg_link(github: NamedTuple, cfg: NamedTuple, pkg: Path) -> str:
+  if github.ref_type != "tag":
+    return f"`{pkg.name}`"
+  url = f"{cfg.build.repository.url}/releases/download/{github.ref_name}/{pkg.name}"
+  return f"[`{pkg.name}`]({url})"
 
 
 def summarize(clone_dir: Path, github: NamedTuple, inputs: NamedTuple, cfg: NamedTuple) -> str:
-  repo = github.repository.split("/")[-1]
-  repo_url = f"https://github.com/{github.repository}"
-
-  settings_link = (
-    f"[settings.yml]({repo_url}/blob/{github.sha}/.github/workflows_pyconfig/settings.yml)"
+  # Read release-tracker commit id
+  reltracker_commit_f = Path(f"{cfg.build.artifacts_dir}/release-tracker.commit")
+  reltracker_commit = reltracker_commit_f.read_text()
+  # Read list of generated images from the release-tracker's summary
+  reltracker_summary_f = Path(f"{cfg.build.artifacts_dir}/release-tracker-summary.json")
+  reltracker_summary = json.loads(reltracker_summary_f)
+  release_docker_manifest_f_rel = Path(
+    f"{reltracker_summary['storage']}/{reltracker_summary['track']}/{reltracker_summary['entry']}"
   )
-
-  def _image_link(image: str) -> str:
-    image_repo = image.split(":")[0]
-    repo_name = image_repo.split("/")[-1]
-    if image.startswith("ghcr.io/"):
-      url = f"{repo_url}/pkgs/container/{repo_name}"
-    elif image.startswith(github.repository):
-      url = f"https://hub.docker.com/r/{image_repo}"
-    else:
-      # unknown registry
-      url = f"https://{image_repo}"
-    return f"[`{image}`]({url})"
-
-  prerel_image_link = _image_link(cfg.release.prerelease_image)
-  rel_images_list = "".join(
-    [
-      "<ul>",
-      *(f"<li>{_image_link(img)}</li>" for img in cfg.release.final_images),
-      "</ul> |",
-    ]
+  release_docker_manifest_f = (
+    Path(f"{github.workspace}/{reltracker_summary['path']}") / release_docker_manifest_f_rel
   )
+  release_docker_manifest = json.loads(release_docker_manifest_f.read_text())
 
-  def _deb_pkg_link(pkg: Path) -> str:
-    if github.ref_type != "tag":
-      return f"`{pkg.name}`"
-    url = f"{repo_url}/releases/download/{github.ref_name}/{pkg.name}"
-    return f"[`{pkg.name}`]({url})"
+  release_docker_layers = {}
+  for img, img_manifest in release_docker_manifest["images"].items():
+    img_layers = release_docker_layers[img] = release_docker_layers.get(img, {})
+    arch_layers = {
+      layer["digest"]: {
+        "image": img,
+        "platform": layer["platform"],
+      }
+      for layer in img_manifest["manifests"]
+      if layer["platform"]["os"] != "unknown"
+    }
+    unknown_layers = {
+      layer["digest"]: {"image": img, "platform": base_platform, "unknown": True}
+      for layer in img_manifest["manifests"]
+      if layer["platform"]["os"] == "unknown"
+      for base_platform in [
+        next(
+          base_layer_cfg["platform"]
+          for base_layer, base_layer_cfg in arch_layers.items()
+          if base_layer == layer["annotations"]["vnd.docker.reference.digest"]
+        )
+      ]
+    }
+    img_layers.update(arch_layers)
+    img_layers.update(unknown_layers)
 
-  artifacts_dir = Path(github.workspace) / "artifacts"
-  deb_packages = list(artifacts_dir.glob("*.deb"))
-  deb_packages_list = "".join(
-    [
-      "<ul>",
-      *(f"<li>{_deb_pkg_link(pkg)}</li>" for pkg in deb_packages),
-      "</ul>",
-    ]
-  )
+  generated_images = set(release_docker_manifest["images"].keys())
+  # generated_images = reduce(lambda r, v: r | set(v), release_docker_manifest["layers"].values(), set())
+  missing_images = set(cfg.release.final_images) - generated_images
 
-  summary_title = f"{cfg.build.profile} release - {repo} - {cfg.build.version}"
+  # Detect generated Debian packages
+  deb_packages = list(cfg.build.artifacts_dir.glob("*.deb"))
+  expected_deb_packages_globs = [
+    f"{cfg.build.repository.name}_*_{arch}.deb" for arch in cfg.debian.builder.architectures
+  ]
+  missing_deb_packages = [
+    matched
+    for glob in expected_deb_packages_globs
+    for matched in [next((pkg for pkg in deb_packages if fnmatch(pkg, glob)), None)]
+    if matched
+  ]
 
-  if github.ref_type == "branch":
-    release_page = "N/A"
-  else:
-    release_page = f"[{github.ref_name}]({repo_url}/releases/tag/{github.ref_name})"
+  missing_section = bool(missing_images or missing_deb_packages)
 
-  return (
-    f"# {summary_title}"
-    "\n"
-    "\n"
-    "## Artifacts"
-    "\n"
-    "\n"
-    f"| Type | Artifacts |"
-    "\n"
-    f"|------|-----------|"
-    "\n"
-    f"| **Pre-release Image** | {prerel_image_link} |"
-    "\n"
-    f"| **Release Images** | {rel_images_list} |"
-    "\n"
-    f"| **Debian Packages** | {deb_packages_list} |"
-    "\n"
-    "\n"
-    "## Configuration"
-    "\n"
-    f"| Property | Value |"
-    "\n"
-    f"|----------|-------|"
-    "\n"
-    f"| **CI Settings** | {settings_link} |"
-    "\n"
-    f"| **Commit** | [`{github.sha}`]({repo_url}/tree/{github.sha}) |"
-    "\n"
-    f"| **GitHub Release** | {release_page} |"
-    "\n"
-  )
+  result = [
+    f"# {cfg.build.profile} release - {cfg.build.repository} - {cfg.build.version}",
+    "",
+    "## Configuration",
+    "",
+    "| Property | Value |",
+    "|----------|-------|",
+    "| **CI Settings** "
+    + f"| [settings.yml]({cfg.build.repository.url}/blob/{github.sha}/{cfg.build.settings_file})"
+    + " |",
+    "| **Commit** | " + f"[`{github.sha}`]({cfg.build.repository.url}/tree/{github.sha})" + " |",
+    "| **GitHub Release** | "
+    + (
+      f"[{github.ref_name}]({cfg.build.repository.url}/releases/tag/{github.ref_name})"
+      if github.ref_type == "branch"
+      else "N/A"
+    )
+    + " |",
+    "| **Log Entry** | "
+    + (
+      f"[{reltracker_summary['entry']}]({cfg.release.tracker.url}/blob/{reltracker_commit}/{reltracker_summary['entry']}/{release_docker_manifest_f_rel})"
+    )
+    + " |",
+    "",
+    "## Artifacts",
+    "",
+    "| Type | Artifacts |",
+    "|------|-----------|",
+    f"| **Pre-release Image** | {_image_link(github, cfg, cfg.release.prerelease_image)} |",
+    "| **Release Images** | "
+    + "".join(
+      [
+        "<ul>",
+        *(f"<li>{_image_link(github, cfg, img)}</li>" for img in generated_images),
+        "</ul> |",
+      ]
+    )
+    + " |",
+    "| **Debian Packages** | "
+    + (
+      "".join(
+        [
+          "<ul>",
+          *(f"<li>{_deb_pkg_link(pkg)}</li>" for pkg in deb_packages),
+          "</ul>",
+        ]
+      )
+      if deb_packages
+      else "No packages were generated."
+    )
+    + " |",
+    "",
+    *(
+      [
+        "## Missing Artifacts",
+        "",
+        "| Type | Artifacts |",
+        "|------|-----------|",
+        "| **Release Images** | "
+        + "".join(
+          [
+            "<ul>",
+            *(f"<li>{_image_link(github, cfg, img)}</li>" for img in missing_images),
+            "</ul> |",
+          ]
+        )
+        + " |",
+        "| **Debian Packages** | "
+        + "".join(
+          [
+            "<ul>",
+            *(f"<li>`{pkg}`</li>" for pkg in missing_deb_packages),
+            "</ul>",
+          ]
+        )
+        + " |",
+        "",
+      ]
+      if missing_section
+      else []
+    ),
+    "## Docker Image Manifests ",
+    "| **Image** | **Manifest** | **Platform** |",
+    "|-----------|--------------|--------------|",
+    *chain.from_iterable(
+      [
+        f"| `{layer['image']}` | `{digest}` | `{layer['platform']['os']}`/`{layer['platform']['architecture']}`{' (unknown)' if layer['unknown'] else ''} |"
+        for digest, layer in release_docker_layers.items()
+      ]
+    ),
+    "",
+  ]
+
+  return "\n".join(result)
