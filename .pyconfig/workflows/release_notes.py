de40b3b04ade69644ab6972e52c20fc5a60ad714
Andrea Sorbini <asorbini@rti.com>
2024-04-26 01:41:37 -0700

    Release notes

--- .pyconfig/workflows/release_notes.py
+++ .pyconfig/workflows/release_notes.py
@@ -85,7 +85,8 @@ def summarize(clone_dir: Path, github: NamedTuple, inputs: NamedTuple, cfg: Name
     img_layers.update(arch_layers)
     img_layers.update(unknown_layers)
 
-  reltracker_log_url = f"{cfg.release.tracker.repository.url}/blob/{reltracker_commit}/{cfg.build.profile}/{release_docker_manifest_f_rel}"
+  reltracker_path = Path(reltracker_summary["path"])
+  reltracker_log_url = f"{cfg.release.tracker.repository.url}/blob/{reltracker_commit}/{release_docker_manifest_f_rel.relative_to(reltracker_path)}"
 
   generated_images = set(release_docker_manifest["images"].keys())
   # generated_images = reduce(lambda r, v: r | set(v), release_docker_manifest["layers"].values(), set())
